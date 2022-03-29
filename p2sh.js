fc586acef94e39db780f3b187e156d08424db605
Samuel Nevarez <35318078+samrock5000@users.noreply.github.com>
2022-03-29 15:44:02 -0500

    Update p2sh.js

--- p2sh.js
+++ p2sh.js
@@ -1,105 +1,64 @@
 const BCHJS = require('@psf/bch-js')
-const BCHN_MAINNET = 'https://bchn.fullstack.cash/v5/'
-const bchjs = new BCHJS({ restURL: BCHN_MAINNET });
-
-
-// var mainnet = bitcoin.networks.mainnet
-
-
-var keyPairs = [
-    'addWif......',
-    'addWif',
-    'addWif',
-    'addWif',
-    'addWif'
-].map(function (wif) { return bchjs.ECPair.fromWIF(wif) })
-var pubKeys = keyPairs.map(function (x) { return x.getPublicKeyBuffer() })
-
-const redeemScript = bchjs.Script.multisig.output.encode(3, pubKeys)
-//Check OP codes
-const p2msToAsm = bchjs.Script.toASM(redeemScript);
-// Hash P2MS Data to Buffer
-const p2sh_to_160 = bchjs.Crypto.hash160(p2msToAsm);
-//P2SH Buffer
-const p2shPubKey = bchjs.Script.scriptHash.output.encode(p2sh_to_160)
-
-const p2shAddress = bchjs.Address.fromOutputScript(p2shPubKey);
-console.log(p2shAddress)
-
-
-
-
-const getRaw = async () => {
-    // let getRawTransaction = await bchjs.RawTransactions.getTxData(txId);
-    // let decodeRawTransaction = await bchjs.RawTransactions.decodeRawTransaction(unsig)
-    // console.log(getRawTransaction.vin[0].scriptSig)
-    console.log(utxos)
-    // return[getRawTransaction.vout]
-}
-// getRaw()
-
-const p2shtxID = '03cbf67f8af7a1f14ea7e41da8f41fe403d5cdb34fdb70537dd5530089ce8625'
-const p2shaddr = 'bitcoincash:pzd8c030jmgxpjtctvwes8v40t0n8mnmec0w60ff6d'
-const recAddr = "bitcoincash:qzsp6xhrg26lfn4lx3p9vszuhjyatj0j0yq65r5axe"
-
-const newTx1 = new bchjs.TransactionBuilder()
-// const pk1 = Buffer.from(bchjs.Crypto.sha256('a9149a7c3e2f96d060c9785b1d981d957adf33ee7bce87'))
-// const scriptHashBuff = Buffer.from(bchjs.Crypto.sha256(p2shtxID))
-
-const makeTx = async () => {
-    try {
-        // const utxos = await bchjs.Electrumx.utxo(p2shaddr)
-        // const utxo = await utxos.utxos[0]
-        const redeem = await redeemScript
-        //newTx1.addInput(utxo.tx_hash, utxo.tx_pos)
-        newTx1.addInput(p2shtxID, 0)
-        newTx1.addOutput(recAddr, 29000) //30784
-
-        newTx1.sign(0, keyPairs[0], redeem, newTx1.hashTypes.SIGHASH_ALL, 0, newTx1.signatureAlgorithms.SCHNORR)
-        newTx1.sign(0, keyPairs[1], redeem, newTx1.hashTypes.SIGHASH_ALL, 0, newTx1.signatureAlgorithms.SCHNORR)
-        newTx1.sign(0, keyPairs[2], redeem, newTx1.hashTypes.SIGHASH_ALL, 0, newTx1.signatureAlgorithms.SCHNORR)
-        // console.log(newTx1.transaction)
-    } catch (err) {
-        console.error('Error in utxos():', err)
-    }
+const BCHN_MAINNET = 'https://api.fullstack.cash/v5/'
  
-
-    // console.log(utxo)
-    // newTx1.addInput(scriptHashBuff,1, sequence = this.DEFAULT_SEQUENCE, redeem ) 
-    // newTx1.addInputScript(0,sigScript)
-
-
-    //  newTx1.addInputScript(newTx1)
-
-    const finTx = newTx1.build()
-    const tx = finTx.toHex()
-
-    // const txBuffer = Buffer.from(tx, 'hex')
-    // const completeTx = new bchjs.TransactionBuilder()
-    // completeTx.addInputScript(0, p2shPubKey)
-    // console.log(tx)
-
-    try {
-        let sendRawTransaction = await bchjs.RawTransactions.sendRawTransaction(tx);
-
-    } catch (err) {
-        console.error('Error in sendRawTransaction():', err)
-        throw err
-    }
-    //  console.log(sendRawTransaction)
-
-    // let seedBuffer = await bchjs.Mnemonic.toSeed(mnemonic);
-    // const hdNodeSeed = bchjs.HDNode.fromSeed(seedBuffer);
-    // const verify = bchjs.HDNode.verify(hdNodeSeed, pk1, sig1);
-    // console.log(newTx1.transaction.tx.ins);
-    // console.log(newTx1.transaction.inputs);
-
-
-
-    // newTx1.transaction.inputs.signatures = sig1
-    // console.log(newTx1.transaction.inputs.signatures);
-    // console.log(newTx1.transaction.tx.ins);
-    // console.log(newTx1.transaction.tx.outs);
-    // console.log(newTx1);
-}
-makeTx()
+const bchjs = new BCHJS({ restURL: BCHN_MAINNET });
+ 
+var keyPairs = [
+    'wif1',
+    'wif2',
+    'wif3',
+    'wif4',
+    'wif5'
+     
+    ].map(function (wif) { return bchjs.ECPair.fromWIF(wif) })
+    var pubKeys = keyPairs.map(function (x) { return x.getPublicKeyBuffer() })
+ 
+    // var pubKeys = [
+ 
+    // ].map(hex => Buffer.from(hex, 'hex'));
+ 
+    var redeemScript =  bchjs.Script.multisig.output.encode(3, pubKeys)
+ 
+    var scriptPubKey =  bchjs.Script.scriptHash.output.encode(bchjs.Crypto.hash160(redeemScript))
+    //Check OP codes
+    const p2msToAsm = bchjs.Script.toASM(redeemScript);
+    // console.log(p2msToAsm)
+    var address = bchjs.Address.fromOutputScript(scriptPubKey)
+    // var multisig = bchjs.Script.multisig.output.decode(redeemScript)
+ 
+    //Spending from this txid
+    const p2shtxID2 = '193303377ce4958ddc89d6a55fed33d5abd950fb7a6fda4f58555995f2385a0b'
+    //To this address
+    const recAddr = "bitcoincash:qp9dg9ddlv4e503ma70awdqgj406et4r8vt9j0svlr"
+   
+    let originalAmount = 13497
+    // let originalAmount = 49925
+    let witnessScript
+    let byteCount = bchjs.BitcoinCash.getByteCount({'MULTISIG-P2SH:3-5': 1},{P2PKH: 1})
+    let sendAmount = originalAmount - byteCount;
+ 
+ 
+        var txb = new bchjs.TransactionBuilder()
+     
+ 
+       txb.addInput(p2shtxID2,0)
+       txb.addOutput(recAddr, sendAmount)
+ 
+       txb.sign(0, keyPairs[0], redeemScript, txb.hashTypes.SIGHASH_ALL, sendAmount, txb.signatureAlgorithms.ECDSA)
+       txb.sign(0, keyPairs[1], redeemScript, txb.hashTypes.SIGHASH_ALL, sendAmount, txb.signatureAlgorithms.ECDSA)
+       txb.sign(0, keyPairs[2], redeemScript, txb.hashTypes.SIGHASH_ALL, sendAmount, txb.signatureAlgorithms.ECDSA)
+    //    txb.sign(0, keyPairs[1], redeemScript, txb.hashTypes.SIGHASH_ALL, sendAmount, txb.signatureAlgorithms.ECDSA)
+    //    console.log(txb)
+       var finTx = txb.build()
+       const tx = finTx.toHex()
+ 
+     
+            const sendToNetwork = async() => {
+                try {
+                    //  let sendRawTransaction = await bchjs.RawTransactions.sendRawTransaction(tx);
+                 } catch (err) {
+                    console.error('Error in sendRawTransaction():', err)
+                throw err
+              }
+            }
+            // sendToNetwork()
